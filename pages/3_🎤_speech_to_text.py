ce9a3646e55e8dc5d03348832c3654b575beeee9
xeven.ai <143064727+xevenai@users.noreply.github.com>
2023-08-30 12:06:39 +0500

    3_🎤_speech_to_text.py

--- pages/3_🎤_speech_to_text.py
+++ pages/3_🎤_speech_to_text.py
@@ -45,7 +45,7 @@ voice,col1,speechtoText,col3,col4=st.columns(5)
 
 with voice:
     if st.button("Voice Input", use_container_width=True):
-        stream = None
+        stream = ""
         try:
             st.write("Listening...")
             p = pyaudio.PyAudio()
